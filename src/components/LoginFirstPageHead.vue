89ecb76dc1d585e58442e9396097e99840ebac5a
hw_008618387062376_01 <0c0c69e7ef80268e1f61c00d89b3cbc8@example.com>
2021-12-02 10:31:32 +0800

    更新LoginFirstPageHead.vue

--- src/components/LoginFirstPageHead.vue
+++ src/components/LoginFirstPageHead.vue
@@ -8,7 +8,7 @@
       </el-col>
       <el-col :span="6">
         <div>
-          <p style="margin-left:0px;font-size:20px;line-height:25px;color:#f56c6c" @click="goIndex">简学独立博客系统</p>
+          <p style="margin-left:0px;font-size:20px;line-height:25px;color:#f56c6c" @click="goindex" >简学独立博客系统</p>
         </div>
       </el-col>
       <el-col :span="10">
@@ -18,34 +18,26 @@
       </el-col>
       <el-col :span="2">
         <div>
-          <el-button style="margin-left:0;margin-top:10px" @click="this.getvalue" type="info">搜索</el-button>
+          <el-button style="margin-left:0;margin-top:10px" type="info" @click="this.getvalue">搜索</el-button>
         </div>
       </el-col>
       <el-col :span="4">
         <div style="display: flex;justify-content:center" v-show="user.hasLogin">
-          <div>
-            <el-link :underline="false" type="danger" style="margin-top: 20px">消息</el-link>
-          </div>
+          <div> <el-link :underline="false" type="danger" style="margin-top: 20px">消息</el-link></div>
           <el-dropdown class="new-dropdown">
-            <div>
-              <el-avatar :size="60" :src='user.avatar' style="margin-left: 20px"></el-avatar>
-            </div>
+            <div><el-avatar :size="60" :src='user.avatar' style="margin-left: 20px"></el-avatar></div>
             <el-dropdown-menu slot="dropdown" v-if="user.hasLogin">
-              <el-dropdown-item @click.native="userInfo">账户中心</el-dropdown-item>
-              <el-dropdown-item @click.native="selfPage">个人中心</el-dropdown-item>
+              <el-dropdown-item @click.native="userinfo">账户中心</el-dropdown-item>
+              <el-dropdown-item @click.native="selfpage">个人中心</el-dropdown-item>
               <el-dropdown-item @click.native="logout">退出登录</el-dropdown-item>
             </el-dropdown-menu>
           </el-dropdown>
-          <div style="margin-left: 20px;margin-top: 20px">{{ user.username }}</div>
-          <el-link v-if="!user.hasLogin">
-            <router-link :to="{name: 'LoginPage'}">点击登陆</router-link>
-          </el-link>
+          <div style="margin-left: 20px;margin-top: 20px">{{user.username}}</div>
+          <el-link v-if="!user.hasLogin"><router-link :to="{name: 'LoginPage'}">点击登陆</router-link></el-link>
         </div>
         <div v-show="!user.hasLogin">
-          <el-button type="danger" style="margin-top:10px" @click="$router.push('/login')" v-show="!hasLogin">登录
-          </el-button>
-          <el-button type="danger" style="margin-top:10px" @click="$router.push('/register')" v-show="!hasLogin">注册
-          </el-button>
+          <el-button type="danger" style="margin-top:10px" @click="$router.push('/login')" v-show="!hasLogin">登录</el-button>
+          <el-button type="danger" style="margin-top:10px" @click="$router.push('/register')" v-show="!hasLogin">注册</el-button>
         </div>
       </el-col>
     </el-row>
@@ -54,8 +46,8 @@
 
 <script>
 export default {
-  name: "LoginFirstPage",
-  data() {
+  name: 'LoginFirstPage',
+  data () {
     return {
       input: '',
       user: {
@@ -65,45 +57,52 @@ export default {
       }
     }
   },
-  activated() {
+  activated () {
     if (this.$store.getters.getUser.userNickname) {
-      //同步昵称
+      // 同步昵称
       this.user.username = this.$store.getters.getUser.userNickname
-      //同步头像
+      // 同步头像
       this.user.avatar = this.$store.getters.getUser.userProfilePhoto
-      //标记已登陆
+      // 标记已登陆
       this.user.hasLogin = true
     }
   },
-  components: {},
+  components: {
+  },
   methods: {
-    logout() {
+    logout () {
       const _this = this
       this.$axios.get('/logout', {
         headers: {
-          "Authorization": localStorage.getItem("token")
+          'Authorization': localStorage.getItem('token')
         }
       }).then((res) => {
         _this.$store.commit('REMOVE_INFO')
         console.log(this.$store.getters.getUser)
-        location.reload();
-        //_this.$router.push('/login')
+        location.reload()
+        // _this.$router.push('/login')
       })
     },
-    userInfo() {
-      this.$router.replace('/user/info')
+    userinfo () {
+      this.$router.replace('/userinfo')
     },
-    goIndex() {
+    goindex () {
       this.$router.replace('/')
     },
-    selfPage() {
-      this.$router.replace('/page/self')
+    selfpage () {
+      this.$router.replace('/smpage')
     },
     getvalue () {
       console.log(this.input)
-      this.$router.replace('/search/' + this.input)
+      if (this.input.length === 0 || this.input.split(' ').join('').length === 0) {
+        this.$router.replace('/')
+      } else {
+        this.$router.replace('/search/' + this.input)
+      }
+      if (!(this.input.length === 0 || this.input.split(' ').join('').length === 0)){
+        location.reload()
+      }
     }
-
   }
 }
 </script>
